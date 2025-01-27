08446b1f2b61446501b9d0965f1dc620dcaa77dc
Gabriel Costa Luiz <53578058+GabrielCostaLuiz@users.noreply.github.com>
2025-01-27 01:09:41 -0300

    feat: added swagger for documentation

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,41 @@ import fastify from 'fastify'
 
 import { transactionsRoutes } from './routes/transactions'
 import cookie from '@fastify/cookie'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import fastifySwagger from '@fastify/swagger'
+import { knex } from './database'
 
 export const app = fastify()
 
 app.register(cookie)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'API REST NODEJS',
+      description:
+        'API REST NODEJS TRANSACTIONS, FIRST CREATE A TRANSACTION TO USE THE OTHER OPTIONS',
+      version: '1.0.0',
+    },
+    components: {
+      // securitySchemes: {
+      //   bearerAuth: {
+      //     type: 'http',
+      //     scheme: 'bearer',
+      //     bearerFormat: 'JWT',
+      //   },
+      // },
+    },
+  },
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.get('/', async () => {
+  await knex('transactions').delete()
+})
 
 app.register(transactionsRoutes, {
   prefix: 'transactions',
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -7,54 +7,83 @@ import { checkSessionIdExists } from '../middlewares/check-session-id-exist'
 export async function transactionsRoutes(app: FastifyInstance) {
   //   app.addHook('preHandler', async (request, reply) => {})
 
-  app.get('/', { preHandler: [checkSessionIdExists] }, async (request) => {
-    const { sessionId } = request.cookies
+  app.get(
+    '/',
+    {
+      preHandler: [checkSessionIdExists],
+      schema: {
+        tags: ['Transactions'],
+        summary: 'List all transactions',
+      },
+    },
+    async (request) => {
+      const { sessionId } = request.cookies
 
-    const transactions = await knex('transactions')
-      .where('session_id', sessionId)
-      .select()
+      const transactions = await knex('transactions')
+        .where('session_id', sessionId)
+        .select()
 
-    return {
-      transactions,
-    }
-  })
-
-  app.get('/:id', { preHandler: [checkSessionIdExists] }, async (request) => {
-    const getTransactionParamsSchema = z.object({
-      id: z.string().uuid(),
-    })
-
-    const query = getTransactionParamsSchema.safeParse(request.params)
-
-    if (!query.success) {
       return {
-        message: 'Invalid id',
-        error: query.error.flatten((issue: ZodIssue) => ({
-          message: issue.message,
-          errorCode: issue.code,
-        })),
+        transactions,
       }
-    }
+    },
+  )
 
-    const { id } = query.data
-
-    const { sessionId } = request.cookies
-
-    const transaction = await knex('transactions')
-      .where({
-        id,
-        session_id: sessionId,
+  app.get(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExists],
+      schema: {
+        tags: ['Transactions'],
+        summary: 'View a transaction',
+        params: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'string',
+              format: 'uuid',
+            },
+          },
+        },
+      },
+    },
+    async (request) => {
+      const getTransactionParamsSchema = z.object({
+        id: z.string().uuid(),
       })
-      .first()
 
-    return {
-      transaction,
-    }
-  })
+      const query = getTransactionParamsSchema.safeParse(request.params)
+
+      if (!query.success) {
+        return {
+          message: 'Invalid id',
+          error: query.error.flatten((issue: ZodIssue) => ({
+            message: issue.message,
+            errorCode: issue.code,
+          })),
+        }
+      }
+
+      const { id } = query.data
+
+      const { sessionId } = request.cookies
+
+      const transaction = await knex('transactions')
+        .where({
+          id,
+          session_id: sessionId,
+        })
+        .first()
+
+      return {
+        transaction,
+      }
+    },
+  )
 
   app.get(
     '/summary',
-    { preHandler: [checkSessionIdExists] },
+    { preHandler: [checkSessionIdExists], schema: { tags: ['Transactions'] } },
     async (request) => {
       const { sessionId } = request.cookies
 
@@ -69,45 +98,70 @@ export async function transactionsRoutes(app: FastifyInstance) {
     },
   )
 
-  app.post('/', async (request, reply) => {
-    const createTransactionBodyScrema = z.object({
-      title: z.string(),
-      amount: z.number(),
-      type: z.enum(['credit', 'debit']),
-    })
-
-    const body = createTransactionBodyScrema.safeParse(request.body)
-
-    if (!body.success) {
-      return reply.status(400).send({
-        message: 'Invalid body',
-        error: body.error.flatten((issue: ZodIssue) => ({
-          message: issue.message,
-          errorCode: issue.code,
-        })),
+  app.post(
+    '/',
+    {
+      schema: {
+        tags: ['Transactions'],
+        summary: 'Create a transaction',
+        body: {
+          type: 'object',
+          required: ['title', 'amount', 'type'],
+          properties: {
+            title: {
+              type: 'string',
+            },
+            amount: {
+              type: 'number',
+            },
+            type: {
+              type: 'string',
+              enum: ['credit', 'debit'],
+            },
+          },
+        },
+      },
+    },
+    async (request, reply) => {
+      const createTransactionBodyScrema = z.object({
+        title: z.string(),
+        amount: z.number(),
+        type: z.enum(['credit', 'debit']),
       })
-    }
 
-    const { title, amount, type } = body.data
+      const body = createTransactionBodyScrema.safeParse(request.body)
 
-    let sessionId = request.cookies.sessionId
+      if (!body.success) {
+        return reply.status(400).send({
+          message: 'Invalid body',
+          error: body.error.flatten((issue: ZodIssue) => ({
+            message: issue.message,
+            errorCode: issue.code,
+          })),
+        })
+      }
 
-    if (!sessionId) {
-      sessionId = randomUUID()
+      const { title, amount, type } = body.data
 
-      reply.cookie('sessionId', sessionId, {
-        path: '/',
-        maxAge: 60 * 60 * 24 * 7, // 7 days
+      let sessionId = request.cookies.sessionId
+
+      if (!sessionId) {
+        sessionId = randomUUID()
+
+        reply.cookie('sessionId', sessionId, {
+          path: '/',
+          maxAge: 60 * 60 * 24 * 7, // 7 days
+        })
+      }
+
+      await knex('transactions').insert({
+        id: randomUUID(),
+        title,
+        amount: type === 'credit' ? amount : amount * -1,
+        session_id: sessionId,
       })
-    }
 
-    await knex('transactions').insert({
-      id: randomUUID(),
-      title,
-      amount: type === 'credit' ? amount : amount * -1,
-      session_id: sessionId,
-    })
-
-    return reply.status(201).send()
-  })
+      return reply.status(201).send()
+    },
+  )
 }
